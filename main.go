909d52aa6087d16b6e8e6d9cc480be9613a96ab0
Alexandru <157019594+shuiali@users.noreply.github.com>
2025-07-02 00:59:24 +0300

    Update main.go

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ const (
 	// Updated funding constants
 	FUNDING_OPEN_DELAY  = 100 * time.Millisecond // Exactly 500ms before funding
 	FUNDING_CLOSE_DELAY = 100 * time.Millisecond // 100ms after funding
-	LIMIT_ORDER_OFFSET  = 0.003                  // 0.3% below/above opening price for limit orders
+	LIMIT_ORDER_OFFSET  = 0.0005                  // 0.3% below/above opening price for limit orders
 
 	FUNDING_TOPIC_PREFIX = "funding."
 )
